704a519dd1af347c8a064a4643e3f1c5ef7f62d7
John Andersen <john.s.andersen@intel.com>
2020-06-16 22:51:29 -0700

    target-i386: adds CR pin CPUID feature bit
    Adds CR pin CPUID feature bit.
    
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- target/i386/cpu.c
+++ target/i386/cpu.c
@@ -904,7 +904,7 @@ static FeatureWordInfo feature_word_info[FEATURE_WORDS] = {
             "kvm-asyncpf", "kvm-steal-time", "kvm-pv-eoi", "kvm-pv-unhalt",
             NULL, "kvm-pv-tlb-flush", NULL, "kvm-pv-ipi",
             "kvm-poll-control", "kvm-pv-sched-yield", NULL, NULL,
-            NULL, NULL, NULL, NULL,
+            "kvm-cr-pin", NULL, NULL, NULL,
             NULL, NULL, NULL, NULL,
             "kvmclock-stable-bit", NULL, NULL, NULL,
             NULL, NULL, NULL, NULL,
@@ -4211,6 +4211,7 @@ static PropValue kvm_default_props[] = {
     { "kvm-asyncpf", "on" },
     { "kvm-steal-time", "on" },
     { "kvm-pv-eoi", "on" },
+    { "kvm-cr-pin", "on" },
     { "kvmclock-stable-bit", "on" },
     { "x2apic", "on" },
     { "acpi", "off" },
@@ -7096,6 +7097,7 @@ static void x86_cpu_initfn(Object *obj)
     object_property_add_alias(obj, "kvm_pv_eoi", obj, "kvm-pv-eoi");
     object_property_add_alias(obj, "kvm_pv_unhalt", obj, "kvm-pv-unhalt");
     object_property_add_alias(obj, "kvm_poll_control", obj, "kvm-poll-control");
+    object_property_add_alias(obj, "kvm_cr_pin", obj, "kvm-cr-pin");
     object_property_add_alias(obj, "svm_lock", obj, "svm-lock");
     object_property_add_alias(obj, "nrip_save", obj, "nrip-save");
     object_property_add_alias(obj, "tsc_scale", obj, "tsc-scale");
